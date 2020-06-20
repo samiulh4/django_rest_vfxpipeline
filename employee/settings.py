c938dd21af5ab49ca5f109cddf7af2e169f7e22f
Md.Sami Ul Haque <samiulh4@gmail.com>
2020-06-20 23:19:48 +0600

    Completed video 11 and tutorial 10, In here Session Auth not working

--- employee/settings.py
+++ employee/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'b9e8w*y*gh=%=t@r=&n$q!kur2r7=d3qv!o9$@-tsfw%k!dwq#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'hrm',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'employee.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -81,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -101,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -115,8 +111,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- hrm/api.py
+++ hrm/api.py
@@ -21,21 +21,34 @@ class UserList(APIView):
 
 class UserDetail(APIView):
 
-    def get(self, request, employee_id):
+    def get_user(self, employee_id):
         try:
             model = Users.objects.get(id=employee_id)
+            return model
         except Users.DoesNotExist:
-            return Response(f'User With {employee_id} Not Found Is Not Found In Database', status=status.HTTP_404_NOT_FOUND)
-        serializer = UsersSerializer(model)
+            return
+
+    def get(self, request, employee_id):
+        if not self.get_user(employee_id):
+            return Response(f'User With {employee_id} Not Found Is Not Found In Database',
+                            status=status.HTTP_404_NOT_FOUND)
+        serializer = UsersSerializer(self.get_user(employee_id))
         return Response(serializer.data)
 
     def put(self, request, employee_id):
-        try:
-            model = Users.objects.get(id=employee_id)
-        except Users.DoesNotExist:
-            return Response(f'User With {employee_id} Not Found Is Not Found In Database', status=status.HTTP_404_NOT_FOUND)
-        serializer = UsersSerializer(model, data=request.data)
+        if not self.get_user(employee_id):
+            return Response(f'User With {employee_id} Not Found Is Not Found In Database',
+                            status=status.HTTP_404_NOT_FOUND)
+        serializer = UsersSerializer(self.get_user(employee_id), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, employee_id):
+        if not self.get_user(employee_id):
+            return Response(f'User With {employee_id} Not Found Is Not Found In Database',
+                            status=status.HTTP_404_NOT_FOUND)
+        model = self.get_user(employee_id)
+        model.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
